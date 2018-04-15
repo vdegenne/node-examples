67d87b8fc85c17b391fd83b2028fcbde9b679603
Valentin Degenne <vdegenne@gmail.com>
2018-04-15 13:01:34 +0000

    net/server update

--- net/src/server.ts
+++ net/src/server.ts
@@ -1,18 +1,19 @@
-import * as net from "net";
+import * as net from 'net';
 
+
+// when a client connects, this function is called
 const server = net.createServer((socket) => {
-  console.log('server is ready');
-  socket.write('hello from the server');
+  console.log('A new client has just connected.');
+  socket.on('data', (data) => console.log(data.toString()));
+  socket.write('SERVER: "Hello!"');
+
+  setTimeout(() => {
+    socket.end('SERVER: "Bye bye"');
+  }, 10000);
+	
 });
 
-server.on('data', (data) => {
-  console.log(data.toString());
-});
-
-server.on('end', () => {
-  console.log('server has stopped');
-});
 
 server.listen(3001, () => {
-  console.log('listening');
+  console.log('Server is listening.');
 })
